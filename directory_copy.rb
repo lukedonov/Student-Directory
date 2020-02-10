3d9a4dfd5d6e9b2cf56d119af41b7d909eb8ce3d
Luke Donovan <lukelamnisos@icloud.com>
2020-02-10 15:50:50 +0000

    exercise 5

--- directory_copy.rb
+++ directory_copy.rb
@@ -1,19 +1,31 @@
 def input_students
-    puts "Please input the name of students"
-    puts "To finish, please hit return twice"
-    #create an empty array 
     students = []
-    # get the first name
+    puts "Please input student information:"
+    puts "To finish, please hit return twice"
+    puts "What is their name?"
     name = gets.chomp
+    puts "What is their height?"
+    height = gets.chomp
+    puts "What is their hobby?"
+    hobby = gets.chomp
+    
     while !name.empty? do
-        #add the student hash to the array
-        students << {name: name, cohort: :november}
+        students << {name: name, cohort: :november, height: height, hobby: hobby}
         puts "Now we have #{students.count} students"
         # get another name
+        puts "Name:"
         name = gets.chomp
+        if name.empty?
+          break
+        end
+        puts "Height:"
+        height = gets.chomp
+        puts "Hobby:"
+        hobby = gets.chomp
     end
     #return students
     students
+    
 end
 
 def print_header
@@ -21,19 +33,11 @@ def print_header
   puts "--------------"
 end
 
-
-# def print_students(students)
-#   students.each.with_index do |s,index|
-#     if s[:name].length < 12
-#       puts  "#{index+1}. #{s[:name]} (#{s[:cohort]} cohort)"
-#     end
-#   end
-# end
-def print_s(students)
-index = 0
-  while index < students.size
-    puts "#{index+1}. #{students[index][:name]} (#{students[index][:cohort]} cohort)"
-    index += 1
+def print_s(s)
+ind = 0
+  while ind < s.size
+    puts "#{ind+1}. #{s[ind][:name]} - Height: #{s[ind][:height]} Hobby: #{s[ind][:hobby]} (#{s[ind][:cohort]} cohort.)"
+    ind += 1
   end
 end
 
