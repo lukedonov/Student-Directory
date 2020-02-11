7e4ccac9fd277b2b714baf791b4ef542dded4e41
Luke Donovan <lukelamnisos@icloud.com>
2020-02-11 14:39:11 +0000

    exercise 8

--- directory_copy.rb
+++ directory_copy.rb
@@ -7,6 +7,7 @@ def input_students
       "febuary",
       "march",
       "april",
+      "may",
       "june",
       "july",
       "august",
@@ -18,19 +19,19 @@ def input_students
     puts "Please input student information:"
     puts "To finish, please hit return twice"
     puts "What is their name?"
-    name = gets.chomp
+    name = gets.strip
     puts "Which cohort?"
-    cohort = gets.chomp.to_sym
+    cohort = gets.strip
    
     while !cohorts.include? cohort.downcase do 
       puts "Please enter a valid cohort"
-      cohort = gets.chomp
+      cohort = gets.strip
     end
 
     puts "What is their height?"
-    height = gets.chomp
+    height = gets.strip
     puts "What is their hobby?"
-    hobby = gets.chomp
+    hobby = gets.strip
     
     while !name.empty? do
         students << {
@@ -58,10 +59,23 @@ def input_students
         puts "Hobby:"
         hobby = gets.chomp
     end
-    #return students
-    students
+    return students
+    sorted = students.sort_by { |k| k[:cohort] }
+    sorted
+    # sorted = {}
+    # students.map do |stu|
+    # cohort = stu[:cohort]
+    #   if sorted[cohort] == nil
+    #     sorted[cohort] = []
+    #   end
+    #   sorted[cohort] << stu
+    # end
     
+    # sorted.each do |key,value|
+    #   key
+    # end
 end
+
 
 def print_header
   puts "The students of Villains Academy".center(40)
@@ -69,20 +83,45 @@ def print_header
 end
 
 def print_s(s)
-ind = 0
-  while ind < s.size
-    puts "#{ind+1}. #{s[ind][:name]} (#{s[ind][:cohort]} cohort)".center(40)
-    puts "Height: #{s[ind][:height]}".center(40)
-    puts "Hobby: #{s[ind][:hobby]}".center(40)
+#   index = 0
+#   stu_arr = student.to_a
+#   while stu_arr[index]
+  
+  
+#   puts "#{stu_arr[index]} cohort"
+# end
+
+
+  # s.map do |key,value|
+  #   puts "#{key} cohort:".center(40)
+  #   puts "Name: #{value.map {|a,b| a[:name]}}".center(40)
+  #   puts "Height: #{value.map {|a,b| a[:height]}}".center(40)
+  #   puts "Hobby: #{value.map {|a,b| a[:hobby]}}".center(40)
+  #   puts ""
+  # end
+
+
+  ind = 0
+  while ind < s.size  
+    a = "#{ind+1}. #{s[ind][:name].capitalize.center(10)}"
+    b = " Height: #{s[ind][:height].center(10)}"
+    c =  " Hobby: #{s[ind][:hobby].capitalize.center(10)} (#{s[ind][:cohort].capitalize.center(0)} cohort)"    
+    puts a + b + c
     ind += 1
   end
+
 end
 
 def print_footer(names)
-  puts "Overall, we have #{names.count} great students".center(40)
+  if names.count == 1 
+  puts "Overall, we have #{names.count} great student".center(40)
+  else 
+    puts "Overall, we have #{names.count} great students".center(40)
+  end
 end
 
 students = input_students
 print_header
 print_s(students)
 print_footer(students)
+# p input_students
