dd416588a58013b4b5d292bac1914d79ceae23e9
Luke Donovan <lukelamnisos@icloud.com>
2020-02-12 10:16:28 +0000

    adding saving to a file method

--- directory.rb
+++ directory.rb
@@ -63,3 +63,14 @@ def interactive_menu
 end
 
 interactive_menu
+
+def save_students
+    file = File.open("students.csv", "w")
+
+    @students.each do |student|
+        student_data = [student[:name]],[student[:cohort]]
+        csv_line = student_data.join(",")
+        file.puts csv_line
+    end
+    file.close
+end
