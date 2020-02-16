eb39bbccf40a0b8225f481c62ca95d7848fac4cd
Luke Donovan <lukelamnisos@icloud.com>
2020-02-16 20:11:00 +0000

    updated quine

--- quine.rb
+++ quine.rb
@@ -1,7 +1,7 @@
 def source_code
   puts __FILE__
-  # puts File.dirname(__FILE__)
-  # puts File.basename(__FILE__)
+  puts File.dirname(__FILE__)
+  puts File.basename(__FILE__)
 end
 
 source_code
